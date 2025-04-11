5e7f83d247effd48b6d278378c6e6e812b439b48
Brandon Alvarez <balvarez325@gmail.com>
2025-04-11 00:14:46 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,10 +1,31 @@
 """
+Brandon Alvarez Repo - Partner 1 - https://github.com/Brandon-Alvarez-03/lab10-BA-JR
+BAlvarez Github Link and UF Email: https://github.com/Brandon-Alvarez-03
+brandon.alvarez@ufl.edu
+
+Jonathan Reyes Repo - Partner 2
+JReyes Github Link and UF Email: jreyes2@ufl.edu, https://github.com/nahtanoJ-dot-exe
+
+
+
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
+# https://github.com/Brandon-Alvarez-03/lab10-BA-JR
+# Partner 1: Brandon Andrew Alvarez
+# Partner 2: Jonathan Reyes
+
 import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot calculate square root of negative number")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
 def add(a, b):
     return a + b
